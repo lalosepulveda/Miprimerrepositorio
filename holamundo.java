5c149e2a2e14d4dca65a2ffa1945ae9e6c85c7c7
lalosepulveda <142032366+lalosepulveda@users.noreply.github.com>
2023-08-11 10:07:07 -0500

    Add files via upload
    este es mi primer proyecto

---
+++ holamundo.java
@@ -0,0 +1,67 @@
+
+
+/**
+ *
+ * @author laura manco
+ */
+
+//clase principal de java
+public class holamundo {
+    public static void main  (String[] args) {
+        System.out.println("Hola laura");
+        
+        
+    }
+            
+        }
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+         
